f48cde9d9f5fc40801f05403c7d08d47d828ce88
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-01-19 13:30:52 +0100

    fixed style

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld
 {
-    private String a;
+    private String name;
     private int variable;
     private int age;
     private int size;
@@ -19,7 +19,7 @@ public class HelloWorld
 
     private String getFromOneToTen() {
         String result = "";
-        for (int i = 0; i < 10; i ++) {
+        for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result = result + i + " ";
             }
@@ -28,14 +28,15 @@ public class HelloWorld
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+                + " Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        name = "Hello mates!";
+        System.out.println(name);
         variable = LocalDate.now().getYear();
         System.out.println("It is "
                 + variable
